68709906d4ad3935b196c1eae31575566defce7f
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-18 06:20:09 -0400

    Make Executible

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from WeatherClass.WeatherClass.weatherclass import WeatherClass
 import os
 
 ZIPCODE = os.environ.get('ZIPCODE')
+KELVIN_CONST = -273.15
 
 weather_location = WeatherClass(ZIPCODE)
 current_weather_slice = weather_location.weather_data['current']
@@ -18,19 +19,19 @@ def draw_main_table() -> Table:
     for conditions in current_weather_slice['weather']:
         conditions_table.add_column(f"{conditions['main']}: {conditions['id']}")
 
-    main_table = Table(title=f"Weather for {ZIPCODE}",
+    main_table = Table(title=f"Weather for {weather_location.geo_data['name']} ({ZIPCODE})",
                        width=100, box=box.MINIMAL_DOUBLE_HEAD)
     main_table.add_column("Location")
     main_table.add_column("Date")
     main_table.add_column("Time")
-    main_table.add_column("Temp")
+    main_table.add_column("Temp (c)")
     main_table.add_column("Humid %")
     main_table.add_column("Clouds")
     main_table.add_column("Currently")
     main_table.add_row(f"{weather_location.lat}, {weather_location.lon}",
                        f"{datetime.datetime.strftime(now, '%y/%m/%d')}",
                        f"{datetime.datetime.strftime(now, '%H:%M')}",
-                       f"{current_weather_slice['temp']}",
+                       f"{round(current_weather_slice['temp'] + KELVIN_CONST, 1)}",
                        f"{current_weather_slice['humidity']}",
                        f"{current_weather_slice['clouds']}", conditions_table)
     return main_table
