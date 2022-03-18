463856151e1ea91711c8d168fbf8bfbcfdf44a22
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-17 20:10:31 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from WeatherClass.WeatherClass.weatherclass import WeatherClass
+import rich
 
-weather_location = WeatherClass(02188)
 
+weather_location = WeatherClass('02188')
 
+print(weather_location.lat, weather_location.lon)
+for conditions in weather_location.weather_data['current']['weather']:
+    print(conditions['main'])
