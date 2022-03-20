50420569e34b43e9bf2469326e85e20ffe5e9dcc
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-20 09:15:56 -0400

    Added metric/imperial switching.
    cleaned up output.
    
    reorganized some code.
    
    ready for testing!

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ from WeatherClass.WeatherClass.weatherclass import WeatherClass
 import os
 
 ZIPCODE = os.environ.get('ZIPCODE')
+UNITS = os.environ.get('UNITS')
 
-weather_location = WeatherClass(ZIPCODE)
+if UNITS == 'imperial':
+    weather_location = WeatherClass(ZIPCODE, 'imperial')
+    temp_symbol = '℉'
+    speed_symbol = 'mph'
+else:
+    weather_location = WeatherClass(ZIPCODE, 'imperial')
+    temp_symbol = '℃'
+    speed_symbol = 'kph'
+
 current_weather_slice = weather_location.weather_data['current']
 hourly_weather_slice = weather_location.weather_data['hourly']
 daily_weather_slice = weather_location.weather_data['daily']
@@ -22,33 +31,43 @@ sunset_dt = datetime.datetime.fromtimestamp(current_weather_slice['sunset'])
 
 def draw_main_table() -> Table:
     main_table = Table(width=140, box=None, padding=0, pad_edge=False)
-    header_table = Table(width=140, box=None, padding=0, pad_edge=False, show_footer=True)
-    today_table = Table(box=box.MINIMAL_HEAVY_HEAD, padding=0, pad_edge=False, width=140)
+    header_table = Table(width=140, box=None, padding=0, pad_edge=False, show_footer=True, show_header=True)
+    today_table = Table(box=box.MINIMAL_HEAVY_HEAD, padding=0, pad_edge=False, width=140, header_style='bold')
     conditions_table = Table(box=None, padding=1, pad_edge=False)
     hourly_conditions_table = Table(box=None, padding=1, pad_edge=False)
     future_conditions_table = Table(box=box.MINIMAL_HEAVY_HEAD, padding=0, pad_edge=False, width=140)
     historic_conditions_table = Table(box=box.MINIMAL_HEAVY_HEAD, padding=0, pad_edge=False, width=140)
 
-    header_text = Text(f"{datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')} // "
-                       f"{current_weather_slice['temp']} c")
-    header_text.stylize('bold')
-    header_table.add_column(header_text)
-    header_table.add_column(f"Currently in {weather_location.geo_data['name']} ->")
-    header_table.add_column(f"Sunset: {datetime.datetime.strftime(sunset_dt, '%H:%M')}")
-    header_table.add_column(f"{daily_weather_slice[0]['weather'][0]['description']}")
-    header_table.add_column(f"{daily_weather_slice[0]['moon_phase']}")
-    header_table.add_column(f"{current_weather_slice['pressure']} mb")
+    header_temp = Text(f"{datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')} // "
+                       f"{current_weather_slice['temp']} {temp_symbol}")
+    header_temp.stylize('bold')
+    header_table.add_column(header_temp)
+    header_location = Text(f"Currently in {weather_location.geo_data['name']} ->")
+    header_location.stylize('bold')
+    header_table.add_column(header_location)
+    header_desc = Text(f"{daily_weather_slice[0]['weather'][0]['description']}")
+    header_desc.stylize('bold')
+    header_table.add_column(header_desc)
+    header_pressure = Text(f"{current_weather_slice['pressure']} mb")
+    header_pressure.stylize('bold')
+    header_table.add_column(header_pressure)
+    header_sunset = Text(f"Sunset: {datetime.datetime.strftime(sunset_dt, '%H:%M')}")
+    header_sunset.stylize('bold')
+    header_table.add_column(header_sunset)
+    header_moon = Text(f"{weather_location.moon_phase_to_string(daily_weather_slice[0]['moon_phase'])}")
+    header_moon.stylize('bold')
+    header_table.add_column(header_moon)
 
     for hourly_conditions in current_weather_slice['weather']:
         conditions_table.add_column(f"{hourly_conditions['main']}")
 
     today_table.add_column("Date", justify="right")
     today_table.add_column("Time", justify="right")
-    today_table.add_column("Temp", justify="right")
-    today_table.add_column("Feels Like", justify="right")
+    today_table.add_column("Temp", justify="left")
+    today_table.add_column("Feels Like", justify="left")
     today_table.add_column("Pressure", justify="right")
     today_table.add_column("Humid %", justify="right")
-    today_table.add_column("Dew Pt", justify="right")
+    today_table.add_column("Dew Pt", justify="left")
     today_table.add_column("Cloud Cover", justify="right")
     today_table.add_column("UVI", justify="right")
     today_table.add_column("Wind S", justify="right")
@@ -56,40 +75,67 @@ def draw_main_table() -> Table:
     today_table.add_column("Forecast")
     today_table.add_row(f"{datetime.datetime.strftime(dt, '%y/%m/%d')}",
                         f"{datetime.datetime.strftime(dt, '%H:%M')}",
-                        f"{current_weather_slice['temp']} c",
-                        f"{current_weather_slice['feels_like']} c",
+                        f"{current_weather_slice['temp']} {temp_symbol} ",
+                        f"{current_weather_slice['feels_like']} {temp_symbol} ",
                         f"{current_weather_slice['pressure']} mb",
                         f"{current_weather_slice['humidity']}",
-                        f"{current_weather_slice['dew_point']} c",
+                        f"{current_weather_slice['dew_point']} {temp_symbol} ",
                         f"{current_weather_slice['clouds']} %",
                         f"{current_weather_slice['uvi']}",
-                        f"{current_weather_slice['wind_speed']} kph",
+                        f"{current_weather_slice['wind_speed']} {speed_symbol}",
                         f"{weather_location.deg_to_direction(current_weather_slice['wind_deg'])}",
                         conditions_table)
-
+    future_conditions_table.add_column("Date", justify="right")
+    future_conditions_table.add_column("High", justify="left")
+    future_conditions_table.add_column("Low", justify="left")
+    future_conditions_table.add_column("Feels Like", justify="left")
+    future_conditions_table.add_column("Pressure", justify="right")
+    future_conditions_table.add_column("Humid %", justify="right")
+    future_conditions_table.add_column("Dew Pt", justify="left")
+    future_conditions_table.add_column("UVI", justify="right")
+    future_conditions_table.add_column("Wind S", justify="right")
+    future_conditions_table.add_column("Wind D", justify="right")
+    future_conditions_table.add_column("Sunrise", justify="right")
+    future_conditions_table.add_column("Moon", justify="right")
+    future_conditions_table.add_column("Forecast")
+    for forecast_data in daily_weather_slice[:7]:
+        dt_forecast = datetime.datetime.fromtimestamp(forecast_data['dt'])
+        dt_sunrise = datetime.datetime.fromtimestamp(forecast_data['sunrise'])
+        future_conditions_table.add_row(f"{datetime.datetime.strftime(dt_forecast, '%y/%m/%d')}",
+                                        f"{forecast_data['temp']['max']} {temp_symbol}",
+                                        f"{forecast_data['temp']['min']} {temp_symbol}",
+                                        f"{forecast_data['feels_like']['day']} {temp_symbol}",
+                                        f"{forecast_data['pressure']} mb",
+                                        f"{forecast_data['humidity']}",
+                                        f"{forecast_data['dew_point']} {temp_symbol}",
+                                        f"{forecast_data['uvi']}",
+                                        f"{forecast_data['wind_speed']} {speed_symbol}",
+                                        f"{weather_location.deg_to_direction(forecast_data['wind_deg'])}",
+                                        f"{datetime.datetime.strftime(dt_sunrise, '%H:%M')}",
+                                        f"{weather_location.moon_phase_to_string(forecast_data['moon_phase'])}",
+                                        f"{weather_location.check_condition(forecast_data['weather'][0]['id'])}")
     for index, hourly_data in zip(range(3), hourly_weather_slice[1:]):
         for hourly_conditions in hourly_data['weather']:
             hourly_conditions_table.add_column(f"{hourly_conditions['main']}")
         dt_old = datetime.datetime.fromtimestamp(hourly_data['dt'])
         today_table.add_row(f"",
                             f"{datetime.datetime.strftime(dt_old, '%H:%M')}",
-                            f"{hourly_data['temp']} c",
-                            f"{hourly_data['feels_like']} c",
+                            f"{hourly_data['temp']} {temp_symbol} ",
+                            f"{hourly_data['feels_like']} {temp_symbol} ",
                             f"{hourly_data['pressure']} mb",
                             f"{hourly_data['humidity']}",
-                            f"{hourly_data['dew_point']} c",
+                            f"{hourly_data['dew_point']} {temp_symbol} ",
                             f"{hourly_data['clouds']} %",
                             f"{hourly_data['uvi']}",
-                            f"{hourly_data['wind_speed']} kph",
+                            f"{hourly_data['wind_speed']} {speed_symbol}",
                             f"{weather_location.deg_to_direction(hourly_data['wind_deg'])}",
                             hourly_conditions_table)
     historic_conditions_table.add_column("Date", justify="right")
-    historic_conditions_table.add_column("Time", justify="right")
-    historic_conditions_table.add_column("Temp", justify="right")
-    historic_conditions_table.add_column("Feels Like", justify="right")
+    historic_conditions_table.add_column("Temp", justify="left")
+    historic_conditions_table.add_column("Feels Like", justify="left")
     historic_conditions_table.add_column("Pressure", justify="right")
     historic_conditions_table.add_column("Humid %", justify="right")
-    historic_conditions_table.add_column("Dew Pt", justify="right")
+    historic_conditions_table.add_column("Dew Pt", justify="left")
     historic_conditions_table.add_column("Cloud Cover", justify="right")
     historic_conditions_table.add_column("UVI", justify="right")
     historic_conditions_table.add_column("Wind S", justify="right")
@@ -102,47 +148,16 @@ def draw_main_table() -> Table:
     for historic_data in historic_slice:
         dt_old = datetime.datetime.fromtimestamp(historic_data['dt'])
         historic_conditions_table.add_row(f"{datetime.datetime.strftime(dt_old, '%y/%m/%d')}",
-                                          f"{datetime.datetime.strftime(dt_old, '%H:%M')}",
-                                          f"{historic_data['temp']} c",
-                                          f"{historic_data['feels_like']} c",
+                                          f"{historic_data['temp']} {temp_symbol} ",
+                                          f"{historic_data['feels_like']} {temp_symbol} ",
                                           f"{historic_data['pressure']} mb",
                                           f"{historic_data['humidity']}",
-                                          f"{historic_data['dew_point']} c",
+                                          f"{historic_data['dew_point']} {temp_symbol} ",
                                           f"{historic_data['clouds']} %",
                                           f"{historic_data['uvi']}",
-                                          f"{historic_data['wind_speed']} kph",
+                                          f"{historic_data['wind_speed']} {speed_symbol}",
                                           f"{weather_location.deg_to_direction(historic_data['wind_deg'])}",
                                           f"{weather_location.check_condition(historic_data['weather'][0]['id'])}")
-    future_conditions_table.add_column("Date", justify="right")
-    future_conditions_table.add_column("High", justify="right")
-    future_conditions_table.add_column("Low", justify="right")
-    future_conditions_table.add_column("Feels Like", justify="right")
-    future_conditions_table.add_column("Pressure", justify="right")
-    future_conditions_table.add_column("Humid %", justify="right")
-    future_conditions_table.add_column("Dew Pt", justify="right")
-    future_conditions_table.add_column("UVI", justify="right")
-    future_conditions_table.add_column("Wind S", justify="right")
-    future_conditions_table.add_column("Wind D", justify="right")
-    future_conditions_table.add_column("Sunrise", justify="right")
-    future_conditions_table.add_column("Moon", justify="right")
-    future_conditions_table.add_column("Forecast")
-    for forecast_data in daily_weather_slice[:4]:
-        dt_forecast = datetime.datetime.fromtimestamp(forecast_data['dt'])
-        dt_sunrise = datetime.datetime.fromtimestamp(forecast_data['sunrise'])
-        future_conditions_table.add_row(f"{datetime.datetime.strftime(dt_forecast, '%y/%m/%d')}",
-                                        f"{forecast_data['temp']['max']} c",
-                                        f"{forecast_data['temp']['min']} c",
-                                        f"{forecast_data['feels_like']['day']} c",
-                                        f"{forecast_data['pressure']} mb",
-                                        f"{forecast_data['humidity']}",
-                                        f"{forecast_data['dew_point']} c",
-                                        f"{forecast_data['uvi']}",
-                                        f"{forecast_data['wind_speed']} kph",
-                                        f"{weather_location.deg_to_direction(forecast_data['wind_deg'])}",
-                                        f"{datetime.datetime.strftime(dt_sunrise, '%H:%M')}",
-                                        f"{weather_location.moon_phase_to_string(forecast_data['moon_phase'])}",
-                                        f"{weather_location.check_condition(forecast_data['weather'][0]['id'])}")
-
     main_table.add_row(header_table)
     main_table.add_row(today_table)
     main_table.add_row(future_conditions_table)
