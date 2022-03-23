ecefccb13456cc31b4e286d3bfee11ff1229d2fa
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-23 12:07:20 -0400

    Adding icons and moving code around.

---
+++ functions.py
@@ -0,0 +1,107 @@
+import random
+
+
+def get_pressure_display(hour_weather_slice):
+    return_pressure_list = []
+    current_pressure = hour_weather_slice[0]['pressure']
+    for index, hour in enumerate(hour_weather_slice):
+        y = hour['pressure']
+        if index == 4 or index == 12 or index == 23:
+            return_pressure_list += slope_calculator(0, index, current_pressure, y)
+    return return_pressure_list
+
+
+def slope_calculator(x1, x2, y1, y2):
+    return_icon = '➡'
+    rx = (y2 - y1) / (x2 - x1)
+    pressure_icons = ['↗️', '↘️']
+    if rx >= .5:
+        return_icon = pressure_icons[0]
+    elif rx <= -.5:
+        return_icon = pressure_icons[1]
+    return return_icon
+
+
+def get_sun_icons(hour, sunrise, sunset):
+    sunrise_icons = ['🌄', '🌅', '🌇']
+    sunset_icons = ['🌃', '🌉', '🌌']
+    if sunset > hour > sunrise:
+        return "am", random.choice(sunset_icons)
+    else:
+        return "pm", random.choice(sunrise_icons)
+
+
+def get_weather_emoji(status_code):
+    emoji_list = ['❌', '⛈', '☔', '🌧', '🌨️', '🌪', '🌞', '🌤', '⛅', '☁']
+    return_code = 0
+    if status_code < 299:
+        return_code = 1
+    elif 300 <= status_code < 399:
+        return_code = 2
+    elif 500 <= status_code < 599:
+        return_code = 3
+    elif 600 <= status_code < 699:
+        return_code = 4
+    elif 700 <= status_code < 781:
+        return_code = 5
+    elif status_code == 800:
+        return_code = 6
+    elif status_code == 801:
+        return_code = 7
+    elif status_code == 802 or status_code == 803:
+        return_code = 8
+    elif status_code == 804:
+        return_code = 9
+    return emoji_list[return_code]
+
+
+def get_time_emoji(hour):
+    hour_icons = ['🕛', '🕐', '🕑', '🕒', '🕓', '🕔' '🕕', '🕖', '🕗', '🕘', '🕙', '🕚']
+    icon = 0
+    if hour == 1:
+        icon = 1
+    elif hour == 2:
+        icon = 2
+    elif hour == 3:
+        icon = 3
+    elif hour == 4:
+        icon = 4
+    elif hour == 5:
+        icon = 5
+    elif hour == 6:
+        icon = 6
+    elif hour == 7:
+        icon = 7
+    elif hour == 8:
+        icon = 8
+    elif hour == 9:
+        icon = 9
+    elif hour == 10:
+        icon = 10
+    elif hour == 11:
+        icon = 11
+    elif hour == 12:
+        icon = 0
+    elif hour == 13:
+        icon = 1
+    elif hour == 14:
+        icon = 2
+    elif hour == 15:
+        icon = 3
+    elif hour == 16:
+        icon = 4
+    elif hour == 17:
+        icon = 5
+    elif hour == 18:
+        icon = 6
+    elif hour == 19:
+        icon = 7
+    elif hour == 20:
+        icon = 8
+    elif hour == 21:
+        icon = 9
+    elif hour == 22:
+        icon = 10
+    elif hour == 23:
+        icon = 11
+    return hour_icons[icon - 1]
--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from rich.live import Live
 from rich.table import Table
 from rich import box
 from rich.text import Text
-from WeatherClass.WeatherClass.weatherclass import WeatherClass
 from newsapi import NewsApiClient
+
+from WeatherClass.WeatherClass.weatherclass import WeatherClass
+from functions import *
 
 ZIPCODE = os.environ.get('ZIPCODE')
 UNITS = os.environ.get('UNITS')
 NEWS_API = os.environ.get("NEWSAPI_KEY")
+PRESSURE_ICONS = ['↗', '➡', '↘']
 SLEEP = 60
+DISPLAY_WIDTH = 140
 
 if UNITS == 'imperial':
     weather_location = WeatherClass(ZIPCODE, 'imperial')
@@ -30,6 +34,15 @@ def draw_main_table() -> Table:
     current_weather_slice = weather_location.weather_data['current']
     hourly_weather_slice = weather_location.weather_data['hourly']
     daily_weather_slice = weather_location.weather_data['daily']
+    # daily_history_slice = weather_location.today_historic_data['daily']
+
+    historic_slice = []
+    while len(daily_weather_slice) <= 1:
+        weather_location.get_historic_weather(len(historic_slice))
+        historic_slice += weather_location.historic_data['hourly'][:1]
+
+    # pressure_icons = get_pressure_display(daily_history_slice)
+    # print(f"{pressure_icons}")
     news = NewsApiClient(api_key=NEWS_API)
     top_headlines = news.get_top_headlines(sources='bbc-news')
 
@@ -37,40 +50,49 @@ def draw_main_table() -> Table:
 
     dt = datetime.datetime.fromtimestamp(current_weather_slice['dt'])
     sunset_dt = datetime.datetime.fromtimestamp(current_weather_slice['sunset'])
+    sunrise_dt = datetime.datetime.fromtimestamp(current_weather_slice['sunrise'])
+    sun_icons = get_sun_icons(dt, sunrise_dt, sunset_dt)
 
     header_style = "bold yellow1"
     main_table_style = "dark_orange"
+    box_style = box.MINIMAL_HEAVY_HEAD
 
-    main_table = Table(width=140, box=None, padding=0, pad_edge=False, style=f"{main_table_style}")
-    header_table = Table(width=140, box=None, padding=0, pad_edge=False, show_footer=True,
+    main_table = Table(width=DISPLAY_WIDTH, box=None, padding=0, pad_edge=False, style=f"{main_table_style}")
+    header_table = Table(width=DISPLAY_WIDTH, box=None, padding=0, pad_edge=False, show_footer=True,
                          show_header=True, style=f"{header_style}")
-    today_table = Table(box=box.MINIMAL_HEAVY_HEAD,
-                        padding=0, pad_edge=False, width=140, row_styles=["grey50", "white"])
-    conditions_table = Table(box=None, padding=1, pad_edge=False, style='italic white')
-    hourly_conditions_table = Table(box=None, padding=1, pad_edge=False, style='italic white')
-    future_conditions_table = Table(box=box.MINIMAL_HEAVY_HEAD,
-                                    padding=0, pad_edge=False, width=140, row_styles=["grey50", "white"])
-    historic_conditions_table = Table(box=box.MINIMAL_HEAVY_HEAD,
-                                      padding=0, pad_edge=False, width=140, row_styles=["grey50", "white"])
+    today_table = Table(box=box_style,
+                        padding=0, pad_edge=False, width=DISPLAY_WIDTH, row_styles=["grey62", "grey93"])
+    conditions_table = Table(box=None, padding=1, pad_edge=False)
+    hourly_conditions_table = Table(box=None, padding=1, pad_edge=False)
+    future_conditions_table = Table(box=box_style,
+                                    padding=0, pad_edge=False, width=DISPLAY_WIDTH, row_styles=["grey62", "grey93"])
+    historic_conditions_table = Table(box=box_style,
+                                      padding=0, pad_edge=False, width=DISPLAY_WIDTH, row_styles=["grey62", "grey93"])
     top_headlines_table = Table(box=None,
-                                padding=0, pad_edge=False, width=140, row_styles=["grey50", "white"])
-
-    header_temp = Text(f"🕐 {datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')} // "
+                                padding=0, pad_edge=False, width=DISPLAY_WIDTH, row_styles=["grey62", "grey93"])
+    header_temp = Text(f"{get_time_emoji(int(datetime.datetime.strftime(datetime.datetime.now(), '%H')))} "
+                       f"{datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')} // "
                        f"🌡️ {current_weather_slice['temp']} {temp_symbol}")
     header_temp.stylize(f"{header_style}")
     header_table.add_column(header_temp)
     header_location = Text(f"Currently in {weather_location.geo_data['name']} ->")
     header_location.stylize(f"{header_style}")
     header_table.add_column(header_location)
-    header_desc = Text(f"🌄 {daily_weather_slice[0]['weather'][0]['description']}")
+    header_desc = Text(f"{get_weather_emoji(int(current_weather_slice['weather'][0]['id']))} "
+                       f"{current_weather_slice['weather'][0]['description']}")
     header_desc.stylize(f"{header_style}")
     header_table.add_column(header_desc)
     header_pressure = Text(f"🌈 {current_weather_slice['pressure']} mb")
     header_pressure.stylize(f"{header_style}")
     header_table.add_column(header_pressure)
-    header_sunset = Text(f"🌇 {datetime.datetime.strftime(sunset_dt, '%H:%M')}")
-    header_sunset.stylize(f"{header_style}")
-    header_table.add_column(header_sunset)
+    if sun_icons[0] == "am":
+        header_sunset = Text(f"{sun_icons[1]} {datetime.datetime.strftime(sunset_dt, '%H:%M')}")
+        header_sunset.stylize(f"{header_style}")
+        header_table.add_column(header_sunset)
+    else:
+        header_sunrise = Text(f"{sun_icons[1]} {datetime.datetime.strftime(sunset_dt, '%H:%M')}")
+        header_sunrise.stylize(f"{header_style}")
+        header_table.add_column(header_sunrise)
     header_moon = Text(f"🌜 {weather_location.moon_phase_to_string(daily_weather_slice[0]['moon_phase'])}")
     header_moon.stylize(f"{header_style}")
     header_table.add_column(header_moon)
@@ -161,12 +183,6 @@ def draw_main_table() -> Table:
     historic_conditions_table.add_column("Wind D", justify="right")
     historic_conditions_table.add_column("Forecast")
 
-    historic_slice = []
-
-    while len(historic_slice) <= 1:
-        weather_location.get_historic_weather(len(historic_slice))
-        historic_slice += weather_location.historic_data['hourly'][:1]
-
     for historic_data in historic_slice:
         dt_old = datetime.datetime.fromtimestamp(historic_data['dt'])
         historic_conditions_table.add_row(f"{datetime.datetime.strftime(dt_old, '%y/%m/%d')}",
@@ -181,24 +197,24 @@ def draw_main_table() -> Table:
                                           f"{weather_location.deg_to_direction(historic_data['wind_deg'])}",
                                           f"{weather_location.check_condition(historic_data['weather'][0]['id'])}")
     articles = top_headlines['articles']
-    top_headlines_table.add_row(f"\tHeadlines ->", style="dark_orange")
+    top_headlines_table.add_row(f"\tHeadlines ->", style=f"{main_table_style}")
     for article in random.sample(articles, 4):
-        top_headlines_table.add_row(f"\t{article['description']}")
+        top_headlines_table.add_row(f"\t{article['description'][:130]}")
 
     main_table.add_row(header_table)
     main_table.add_row(today_table)
     main_table.add_row(future_conditions_table)
     main_table.add_row(historic_conditions_table)
     main_table.add_row(top_headlines_table)
-
     return main_table
 
 
 def main():
-    with Live(draw_main_table(), refresh_per_second=4) as live:
+    with Live(draw_main_table(), refresh_per_second=1) as live:
         while True:
-            live.update(draw_main_table())
+            weather_location.get_today_history()
             weather_location.update_weather()
+            live.update(draw_main_table())
             time.sleep(SLEEP)
 
 
